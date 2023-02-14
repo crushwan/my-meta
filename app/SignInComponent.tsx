ca4831068ee913c2ce03771c650c635fd25bf135
crushwan <crushwan@gmail.com>
2023-02-15 04:19:26 +0800

    "try"

--- app/SignInComponent.tsx
+++ app/SignInComponent.tsx
@@ -8,24 +8,23 @@ type Props = {
 function SignInComponent({ providers }: Props) {
   return (
     <div className="flex justify-center">
-      {/* {Object.values(providers!).map((provider) => ( */}
-      {/* <div key={provider.name}> */}
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2
+      {Object.values(providers!).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2
             px-4 rounded"
-        onClick={
-          () => signIn("google")
-          // signIn(provider.id, {
-          //   callbackUrl:
-          //     process.env.NEXTAUTH_URL || "http://localhost:3000",
-          // })
-        }
-      >
-        {/* Sign in with {provider.name} */}
-        Sign in with
-      </button>
-      {/* </div> */}
-      {/* ))} */}
+            onClick={() =>
+              signIn(provider.id, {
+                callbackUrl:
+                  `${process.env.NEXTAUTH_URL}//api/auth/callback/google` ||
+                  "http://localhost:3000",
+              })
+            }
+          >
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
